c5c9f53860fb9eb30de3d10256691fa318edc211
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-12-06 20:59:11 +0500

    Ship sunk or not testing: Write test to check if ship has been sunk before hits and also after hits

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,8 +1,15 @@
 // ship.test.js
 import {ship} from "../factories/ship"
-test('How many times ship has been hit', () => {
+test("How many times ship's been hit?", () => {
     const myship = ship();
     expect(myship.timesHit()).toBe(0);
     myship.hit();
     expect(myship.timesHit()).toBe(1);
+})
+test("Is ship sunk?", () => {
+    const myship = ship(2);
+    expect(myship.isSunk()).toBe(false);
+    myship.hit();
+    myship.hit();
+    expect(myship.isSunk()).toBe(true);
 })
