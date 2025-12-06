852ebe169c3e1b6e5a1e14a81324802ae491fb65
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-12-06 21:06:47 +0500

    Ship sunk function: Write function when ship will sink

--- src/factories/ship.js
+++ src/factories/ship.js
@@ -1,11 +1,18 @@
 //ship.js
-function ship() {
+function ship(length = 1) {
     let shipHitTimes = 0;
     const timesHit = () => shipHitTimes;
     const hit = () => shipHitTimes++;
+    const isSunk = () => {
+        if(shipHitTimes >= length){
+            return true;
+        }
+        return false;
+    }
     return {
         hit,
-        timesHit
+        timesHit,
+        isSunk
     }
 }
 export {ship}
