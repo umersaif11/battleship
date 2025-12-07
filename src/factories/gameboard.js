a5f092c035e7dcab6d66575a26b77794b696a9ab
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-12-07 18:10:49 +0500

    Gameboard placeship/recieve attack functions: 1. First make the placeship function which takes ships length and coordinates and saves this in an array. 2. receiveAttack searches through this master array which is global and checks if currently this coordinates are present and if yes return true.

---
+++ src/factories/gameboard.js
@@ -0,0 +1,52 @@
+//gameboard.js
+import {ship} from "../factories/ship"
+function gameboard() {
+    let shipsWithPositions;
+    const areArraysEqual = (arr1, arr2) => {
+        if (arr1.length !== arr2.length) {
+            return false;
+        }
+        for (let i = 0; i < arr1.length; i++) {
+            if (arr1[i] !== arr2[i]) {
+            return false;
+            }
+        }
+        return true;
+    }
+    const placeShip = (coordinates, length) => {
+        const myship = ship(length);
+        shipsWithPositions = [{
+            myship,
+            shiplength: myship.length,
+            shipcoordinates: []
+        }];
+        for(let ship of shipsWithPositions) {
+            if(ship.shiplength === length) {
+                ship.shipcoordinates.push(coordinates)
+            } else {
+                shipsWithPositions.push({
+                    myship,
+                    shiplength: myship.length,
+                    shipcoordinates: []
+                })
+            }
+        }
+        return shipsWithPositions;
+        
+    }
+    const receiveAttack = (attackCoordinates) => {
+        for(let ship of shipsWithPositions) {
+            for(let coordinate of ship.shipcoordinates) {
+                if(areArraysEqual(coordinate, attackCoordinates)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    return {
+        placeShip,
+        receiveAttack
+    }
+}
+export {gameboard}
