ba65bb020495edf14a76d28187dc58c1147223dc
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-12-07 21:08:20 +0500

    Gameboard's placeship testing: Test whether current function can calculate next coordinates of a ship

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,21 +1,25 @@
 //gameboard.test.js
 import {gameboard} from "../factories/gameboard"
 
-test('Did ship receive attack?', () => {
+test('Did receiveAttack() receive attack?', () => {
     const mygameboard = gameboard();
     mygameboard.placeShip([0,3], 2);
     expect(mygameboard.receiveAttack([0,3])).toBe(true);
 })
-test('Can place multiple ships?', () => {
+test('Can placeShip() place multiple ships?', () => {
     const mygameboard = gameboard();
     mygameboard.placeShip([0, 0], 3);
     mygameboard.placeShip([0, 4], 4);
     expect(mygameboard.receiveAttack([0, 0])).toBe(true);
 })
-// test('Can placeShip() calculates next coordinates?', () =)
-test('Can two ships of same length exist?', () => {
+test('Can placeShip() place two ships of same length?', () => {
     const mygameboard = gameboard();
     mygameboard.placeShip([0, 0], 3);
     mygameboard.placeShip([0, 4], 3);
     expect(mygameboard.shipsWithPositions.length).toBe(2);
+})
+test('Can placeShip() calculates next coordinates?', () => {
+    const mygameboard = gameboard();
+    mygameboard.placeShip([0, 0], 3);
+    expect(mygameboard.shipsWithPositions[0].shipcoordinates.length).toBe(3);
 })
