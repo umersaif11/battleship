8cc5f8e99e814a5769de3e9863799c347bf90eb6
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-12-07 19:17:38 +0500

    Gameboard's placeship testing: Add the test to check if the function can handle multiple inputs which it didnt so this bug has to be removed

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -3,6 +3,12 @@ import {gameboard} from "../factories/gameboard"
 
 test('Did ship receive attack?', () => {
     const mygameboard = gameboard();
-    mygameboard.placeShip([0,3]);
+    mygameboard.placeShip([0,3], 2);
     expect(mygameboard.receiveAttack([0,3])).toBe(true);
+})
+test('Can place multiple ships?', () => {
+    const mygameboard = gameboard();
+    mygameboard.placeShip([0, 0], 3);
+    mygameboard.placeShip([0, 4], 3);
+    expect(mygameboard.receiveAttack([0, 0])).toBe(true);
 })
