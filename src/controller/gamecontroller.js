577eeec8bf1c23fa04821a396b995c562ed1b7ed
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-12-08 20:02:14 +0500

    Gamecontroller: Write start game func which for now takes two players manually with pre determined ships positions

---
+++ src/controller/gamecontroller.js
@@ -0,0 +1,16 @@
+//gamecontroller.js
+import {player} from "../factories/player"
+
+function startGame() {
+    const human = player('real');
+    const computer = player('computer');
+    human.gameboard.placeShip([0, 0], 2, 'x');
+    human.gameboard.placeShip([1, 1], 2, 'y');
+    computer.gameboard.placeShip([2, 0], 2, 'y');
+    computer.gameboard.placeShip([1, 2], 2, 'x');
+    return {
+        human,
+        computer
+    }
+}
+export {startGame}
