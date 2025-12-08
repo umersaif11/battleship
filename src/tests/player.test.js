7ac7a87d16f50ac4c9b542b68a5b8c2d2142f46c
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-12-08 15:31:40 +0500

    Player factory testing: Can a factory return a specified type and has also the gameboard properties?

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -8,3 +8,8 @@ test('Create computer player', () => {
     const computerPlayer = player('computer');
     expect(computerPlayer.type).toBe('computer');
 })
+test('Is gameboard on player func defined and has all its functions?', () => {
+    const myPlayer = player('real');
+    expect(myPlayer.gameboard).toBeDefined();
+    expect(myPlayer.gameboard).toHaveProperty('placeShip');
+})
