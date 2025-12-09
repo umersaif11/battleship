01869e9e8d67f2b1ae41ce164f7f2bde1c3a5cb0
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-12-09 15:57:47 +0500

    Gamecontroller: Change coordinates of one ship

--- src/controller/gamecontroller.js
+++ src/controller/gamecontroller.js
@@ -5,7 +5,7 @@ function startGame() {
     const human = player('real');
     const computer = player('computer');
     human.gameboard.placeShip([0, 0], 2, 'x');
-    human.gameboard.placeShip([1, 1], 2, 'y');
+    human.gameboard.placeShip([2, 2], 2, 'y');
     computer.gameboard.placeShip([2, 0], 2, 'y');
     computer.gameboard.placeShip([1, 2], 2, 'x');
     return {
