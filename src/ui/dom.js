69b2ea4ca1fdb2a737ae63dffd65b3c7d46f5ae4
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-12-09 18:46:29 +0500

    DOM func within areArraysEqual comparison: as i is changing when moves down on screen grid so i represents y-axis as i is changing in vertical direction, so i must be placed at yth position i.e second as [j, i] instead of [i, j]

--- src/ui/dom.js
+++ src/ui/dom.js
@@ -10,10 +10,6 @@ function renderBoard(htmlId, gameboardObject) {
             if(!htmlId.includes('computer')) {
                 gameboardObject.shipsWithPositions.forEach(ship => {
                     ship.shipcoordinates.forEach(coordinate => {
-                        //as i is changing when moves down on screen grid
-                        //so i represents y-axis as i is changing in vertical
-                        //direction, so i must be placed in this bracket [] at 
-                        //yth position i.e second
                         if(gameboardObject.areArraysEqual(coordinate, [j, i])) {
                             div.classList.add('ship');
                         }
