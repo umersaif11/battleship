b6ecc6f51c00947c3caed7aa8bdee53090729c3d
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-12-09 15:58:24 +0500

    dom: Flip the order of i and j for the ui

--- src/ui/dom.js
+++ src/ui/dom.js
@@ -1,15 +1,16 @@
 //dom.js
 function renderBoard(htmlId, gameboardObject) {
     const container = document.getElementById(`${htmlId}`);
-
+    container.innerText = '';
     for(let i = 0; i < 10; i++) {
         for(let j = 0; j < 10; j++) {
             const div = document.createElement("div");
             div.classList.add('grid-square');
+            div.innerText = `${j}, ${i}`;
             if(!htmlId.includes('computer')) {
                 gameboardObject.shipsWithPositions.forEach(ship => {
                     ship.shipcoordinates.forEach(coordinate => {
-                        if(gameboardObject.areArraysEqual(coordinate, [i, j])) {
+                        if(gameboardObject.areArraysEqual(coordinate, [j, i])) {
                             div.classList.add('ship');
                         }
                     })
