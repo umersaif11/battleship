acfee3a940a8f5f67430588dd59f97d3faa69884
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-12-09 13:41:59 +0500

    RenderBoard func: Create the function to render the players boards

--- src/ui/dom.js
+++ src/ui/dom.js
@@ -1,1 +1,22 @@
 //dom.js
+function renderBoard(htmlId, gameboardObject) {
+    const container = document.getElementById(`${htmlId}`);
+
+    for(let i = 0; i < 10; i++) {
+        for(let j = 0; j < 10; j++) {
+            const div = document.createElement("div");
+            div.classList.add('grid-square');
+            if(!htmlId.includes('computer')) {
+                gameboardObject.shipsWithPositions.forEach(ship => {
+                    ship.shipcoordinates.forEach(coordinate => {
+                        if(gameboardObject.areArraysEqual(coordinate, [i, j])) {
+                            div.classList.add('ship');
+                        }
+                    })
+                })
+            }
+            container.appendChild(div);
+        }
+    }
+}
+export {renderBoard}
