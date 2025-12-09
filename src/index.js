4fef62889f2f79825af8e95cc99f7214f3f8e88a
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-12-09 13:54:17 +0500

    Import and render board: Import the functions of renderboard() and startGame() and make both boards

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 // index.js
 import "./styles.css";
+import {startGame} from "./controller/gamecontroller"
+import {renderBoard} from "./ui/dom"
+
+const players = startGame();
+
+renderBoard('human-board', players.human.gameboard);
+renderBoard('computer-board', players.computer.gameboard);
 
 
   
