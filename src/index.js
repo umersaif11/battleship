2cc684b5c2bafd93673713745be7ec30270f8902
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-12-10 12:54:56 +0500

    Battleship main conroller: Make the one function main that will contain all other functions

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import "./styles.css";
 import {startGame} from "./controller/gamecontroller"
 import {renderBoard} from "./ui/dom"
 
-const players = startGame();
+function main() {
+    const players = startGame();
+    const renderUI = () => {
+        renderBoard('human-board', players.human.gameboard);
+        renderBoard('computer-board', players.computer.gameboard, (coordinates) => {
+            console.log("Attack recieved at:", coordinates);
+        });
+    }
 
-renderBoard('human-board', players.human.gameboard);
-renderBoard('computer-board', players.computer.gameboard, (coordinates) => {
-    console.log("Attack recieved at:", coordinates);
-});
+    renderUI();
+}
+main();
 
 
   
