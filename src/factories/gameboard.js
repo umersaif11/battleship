182313b95d052ee9dbaab0ce38fcd01f54f3a66c
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-12-14 13:16:50 +0500

    Gameboard alignment issue resolved

--- src/factories/gameboard.js
+++ src/factories/gameboard.js
@@ -7,7 +7,7 @@ function gameboard() {
         }
         for (let i = 0; i < arr1.length; i++) {
             if (arr1[i] !== arr2[i]) {
-            return false;
+                return false;
             }
         }
         return true;
