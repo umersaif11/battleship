b8561b3d6d9a37c3b849706e82a96d8960e4ec99
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-12-15 17:19:32 +0500

    Game controller's start game func: Make the placement of computer ships random every time game starts

--- src/controller/gamecontroller.js
+++ src/controller/gamecontroller.js
@@ -5,8 +5,19 @@ function startGame() {
     const human = player('real');
     const computer = player('computer');
     
-    computer.gameboard.placeShip([2, 0], 2, 'y');
-    computer.gameboard.placeShip([1, 2], 2, 'x');
+    const shipsLength = [5, 4, 3, 3, 2];
+    shipsLength.forEach(length => {
+        let placed = false;
+        while(!placed) {
+            const randomX = Math.floor(Math.random() * 10);
+            const randomY = Math.floor(Math.random() * 10);
+            const randomAxis = Math.random() < 0.5 ? 'x' : 'y';
+
+            if(computer.gameboard.placeShip([randomX, randomY], length, randomAxis)) {
+                placed = true;
+            }
+        }
+    })
     return {
         human,
         computer
