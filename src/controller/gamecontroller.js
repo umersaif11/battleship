8530b4dc81755ce82d0399f62a915604046c25df
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2025-12-15 13:55:22 +0500

    Gamecontroller: Remove the manual placement of ships

--- src/controller/gamecontroller.js
+++ src/controller/gamecontroller.js
@@ -4,8 +4,7 @@ import {player} from "../factories/player"
 function startGame() {
     const human = player('real');
     const computer = player('computer');
-    human.gameboard.placeShip([0, 0], 2, 'x');
-    human.gameboard.placeShip([2, 2], 2, 'y');
+    
     computer.gameboard.placeShip([2, 0], 2, 'y');
     computer.gameboard.placeShip([1, 2], 2, 'x');
     return {
